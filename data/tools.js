a4490f98327880d992e289f31ca0ca10ff0fe077
coder121 <74172416+shakibuser@users.noreply.github.com>
2026-08-12 16:11:45 +0330

    Update tools.js

--- data/tools.js
+++ data/tools.js
@@ -1,1 +1,15 @@
+const tools = [
+    {
+        title: "محاسبه BMI",
+        icon: "⚖️",
+        description: "محاسبه شاخص توده بدنی و محدوده وزن مناسب",
+        url: "tools/bmi.html"
+    },
 
+    {
+        title: "یادآور",
+        icon: "🧠",
+        description: "یک بازی ساده و سرگرم‌کننده برای تقویت حافظه",
+        url: "tools/memory.html"
+    }
+];

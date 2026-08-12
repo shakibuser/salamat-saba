a28735ee7a8714777e4d9afe9c5b8701bf8c179e
coder121 <74172416+shakibuser@users.noreply.github.com>
2026-08-12 16:53:25 +0330

    Update tools.js

--- data/tools.js
+++ data/tools.js
@@ -1,15 +1,24 @@
 const tools = [
+
     {
-        title: "محاسبه BMI",
+        title: "محاسبه شاخص توده بدنی",
         icon: "⚖️",
-        description: "محاسبه شاخص توده بدنی و محدوده وزن مناسب",
+        description: "قد و وزن خود را وارد کنید و وضعیت وزن خود را ببینید.",
         url: "tools/bmi.html"
+    },
+
+    {
+        title: "بازنشسته‌سنج صبا",
+        icon: "🧓",
+        description: "ببین سبک زندگی‌ات چند ساله است، نه فقط شناسنامه‌ات!",
+        url: "tools/retiree-age.html"
     },
 
     {
         title: "یادآور",
         icon: "🧠",
-        description: "یک بازی ساده و سرگرم‌کننده برای تقویت حافظه",
+        description: "یک بازی سرگرم‌کننده برای به چالش کشیدن حافظه.",
         url: "tools/memory.html"
     }
+
 ];
